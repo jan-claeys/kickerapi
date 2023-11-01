62b69ab462e0c4d2ec5840b42de665969e244d60
AzureAD\JanClaeys <jan.claeys@tillit.be>
2023-11-01 18:18:43 +0100

    Add cors

--- kickerapi/Program.cs
+++ kickerapi/Program.cs
@@ -12,6 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+               builder => builder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                             .AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -92,6 +100,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthentication();
 app.UseAuthorization();
