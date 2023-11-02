a7ee5876c1738062c505fc6837a1b52db05703ad
AzureAD\JanClaeys <jan.claeys@tillit.be>
2023-11-02 16:16:38 +0100

    change default paging items to 20

--- kickerapi/QueryParameters/PagingParameters.cs
+++ kickerapi/QueryParameters/PagingParameters.cs
@@ -4,7 +4,7 @@
     {
         const int maxPageSize = 50;
         public int PageNumber { get; set; } = 1;
-        private int _pageSize = 15;
+        private int _pageSize = 20;
         public int PageSize
         {
             get
