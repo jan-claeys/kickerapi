1206c10c2355a9cd32c33a03d97cf0da4c96df2a
AzureAD\JanClaeys <jan.claeys@tillit.be>
2024-01-31 14:20:21 +0100

    Publish on azure

---
+++ kickerapi/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IStatusCodeActionResult> Get()
+        {
+            return Ok("It works");
+        }
+
+    }
+}
