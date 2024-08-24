530f7c334a40282602bcbfd4c2a02ec32ac8e546
AzureAD\JanClaeys <jan.claeys@tillit.be>
2024-08-24 19:02:18 +0200

    Add adminservice

---
+++ kickerapi/Services/AdminService.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary.Models;
+using kickerapi;
+using kickerapi.Services;
+
+namespace Api.Services
+{
+    public class AdminService : ContextService, IAdminService
+    {
+
+        public AdminService(KickerContext context) :  base(context)
+        {
+        }
+
+        public IQueryable<Match> GetMatchesToReview()
+        {
+            return _context.Matches.Where(m => m.IsCalculatedInRating == false).OrderBy(x=>x.Date);
+        }
+    }
+}
---
+++ kickerapi/Services/IAdminService.cs
@@ -0,0 +1,6 @@
+﻿namespace Api.Services
+{
+    public interface IAdminService
+    {
+    }
+}
